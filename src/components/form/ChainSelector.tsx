0c404496d8e4e995bb80795ea8ee45786cc20427
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-12-09 11:12:46 +0100

    useMemo when calculating the chain list

--- src/components/form/ChainSelector.tsx
+++ src/components/form/ChainSelector.tsx
@@ -1,5 +1,5 @@
 import { useNetwork } from "data/wallet"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import styles from "./ChainSelector.module.scss"
 
 interface Props {
@@ -9,13 +9,17 @@ interface Props {
 
 const ChainSelector = ({ chainsList, onChange }: Props) => {
   const networks = useNetwork()
-  const list = Object.values(networks)
-    .filter((c) => chainsList.includes(c.chainID))
-    .sort((a, b) => {
-      if (a.name === "Terra") return -1
-      if (b.name === "Terra") return 1
-      return 0
-    })
+  const list = useMemo(
+    () =>
+      Object.values(networks)
+        .filter((c) => chainsList.includes(c.chainID))
+        .sort((a, b) => {
+          if (a.name === "Terra") return -1
+          if (b.name === "Terra") return 1
+          return 0
+        }),
+    [networks]
+  )
   const [chain, setChain] = useState(list[0].chainID)
 
   useEffect(() => {
